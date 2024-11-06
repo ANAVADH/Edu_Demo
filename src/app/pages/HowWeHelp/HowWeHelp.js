2eeb1fbb3ffc2fd346b071407e9f69b59179c207
ANAVADH <anavadhsuresh1042@gmail.com>
2024-11-06 13:44:39 +0530

    changes

--- src/app/pages/HowWeHelp/HowWeHelp.js
+++ src/app/pages/HowWeHelp/HowWeHelp.js
@@ -29,29 +29,39 @@ function HowWeHelp() {
           </div>
         </div>
 
-        <div className="flex-1 p-4 justify-center mt-5 md:ml-10">
-          <div className="flex flex-col">
+        <div className="flex-1 p-4 mt-5 md:ml-10">
+  <div className="flex flex-col">
 
-            <div className='flex items-start'>
-              <hr className="w-0.5 h-40 mx-2 my-2 bg-gray-700 border-0 rounded dark:bg-gray-700" />
-              <div className="flex flex-col gap-3 mt-5 ml-5">
-                <h6 className='font-bold'>Lorem ipsum</h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Donec mattis velit nulla. In hac habitasse platea dictumst.<br /> Fusce non venenatis velit. Proin luctus malesuada mauris <br /> vitae mattis. Pellentesque iaculis elit lorem.</p>
-              </div>
-            </div>
+    <div className="flex items-start">
+      <hr className="w-0.5 h-32 md:h-40 mx-2 my-2 bg-gray-700 border-0 rounded dark:bg-gray-700" />
+      <div className="flex flex-col gap-3 mt-3 md:mt-5 ml-2 md:ml-5">
+        <h6 className="font-bold text-base md:text-lg">Lorem ipsum</h6>
+        <p className="text-sm md:text-base leading-snug md:leading-normal">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+          Donec mattis velit nulla. In hac habitasse platea dictumst.<br />
+          Fusce non venenatis velit. Proin luctus malesuada mauris<br />
+          vitae mattis. Pellentesque iaculis elit lorem.
+        </p>
+      </div>
+    </div>
 
-            <hr className="ml-2 w-96 h-0.5 my-4 bg-gray-200 rounded md:my-6 dark:bg-gray-700" />
+    <hr className="w-full md:w-96 h-0.5 my-3 md:my-6 bg-gray-200 rounded dark:bg-gray-700" />
 
-            <div className='flex gap-2 items-start'>
-              <hr className="w-0.5 h-40 mx-2 my-2 bg-gray-700 border-0 rounded dark:bg-gray-700" />
-              <div className="flex flex-col gap-3 mt-5 ml-5">
-                <h6 className='font-bold'>Lorem ipsum</h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Donec mattis velit nulla. In hac habitasse platea dictumst.<br /> Fusce non venenatis velit. Proin luctus malesuada mauris <br /> vitae mattis. Pellentesque iaculis elit lorem.</p>
-              </div>
-            </div>
+    <div className="flex items-start">
+      <hr className="w-0.5 h-32 md:h-40 mx-2 my-2 bg-gray-700 border-0 rounded dark:bg-gray-700" />
+      <div className="flex flex-col gap-3 mt-3 md:mt-5 ml-2 md:ml-5">
+        <h6 className="font-bold text-base md:text-lg">Lorem ipsum</h6>
+        <p className="text-sm md:text-base leading-snug md:leading-normal">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+          Donec mattis velit nulla. In hac habitasse platea dictumst.<br />
+          Fusce non venenatis velit. Proin luctus malesuada mauris<br />
+          vitae mattis. Pellentesque iaculis elit lorem.
+        </p>
+      </div>
+    </div>
 
-          </div>
-        </div>
+  </div>
+</div>
       </div>
     </div>
   )
